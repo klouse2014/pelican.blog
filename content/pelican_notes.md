08bdec8ca52ee3fa43368e57788435804d8e00df
tpwABC <klouse@126.com>
2015-04-25 17:15:18 +0800

    try

---
+++ content/pelican_notes.md
@@ -0,0 +1,20 @@
+Title: 建站学习 
+Date: 2015-04-25 10:20
+Category: Review
+
+##### Pelican参考
+* [主页](http://docs.getpelican.com/en/3.5.0/quickstart.html)
+* [建站实例](http://www.lizherui.com/pages/2013/08/17/build_blog.html)
+* [MarkDown入门指南](http://www.jianshu.com/p/1e402922ee32/)
+* [建造实例2](http://www.cnblogs.com/ballwql/p/pelican.html)
+
+
+##### Elegate主题
+* [配置文档](http://oncrashreboot.com/elegant-best-pelican-theme-features#about-me)
+* [主页](http://oncrashreboot.com/)
+* [setting说明](http://docs.getpelican.com/en/3.2/settings.html)
+
+##### Git branch总结
+
+
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -32,7 +32,31 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = False
 
-THEME = '/home/tpw1988/pelican-themes/blueidea'
+THEME = '/home/tpw1988/pelican-themes/elegant'
+
+PLUGIN_PATH = './pelican-plugins'
+PLUGINS = ['sitemap',]
+MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
+DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
+STATIC_PATHS = ['theme/images', 'images']
+TAG_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+
+#home 配置
+LANDING_PAGE_ABOUT = {'title': 'ACMer',
+                      'details': 'tangpeiwen'}
+PROJECTS = [{
+    'name': 'Logpad + Duration',
+    'url': 'https://github.com/talha131/logpad-plus-duration#logpad--duration',
+    'description': 'Vim plugin to emulate Windows Notepad logging feature,'
+    ' and log duration of each entry'},
+    {'name': 'Elegant Theme for Pelican',
+    'url': 'http://oncrashreboot.com/pelican-elegant',
+    'description': 'A clean and distraction free theme, with search and a'
+    ' lot more unique features, using Jinja2 and Bootstrap'}]
+
+RECENT_ARTICLES_COUNT = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
